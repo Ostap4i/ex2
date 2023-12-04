da7d5b6763980f85662290694932a010e9606592
Ostap4i <137522973+Ostap4i@users.noreply.github.com>
2023-12-04 19:47:14 +0200

    Add files via upload

---
+++ lesson 6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_6
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
